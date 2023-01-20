b079726502c256393a3ad3f47d8a7a5be3f723c7
Soe Paing <sopykt09@gmail.com>
2023-01-20 12:24:24 +0630

    add first template

--- pages/views.py
+++ pages/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(request)
-    print(request.method)
-    print(request.path)
-    print(request.path_info)
-    print(request.COOKIES)
-    print(request.META)
-    return HttpResponse('<h2>Hello World</h2>')
+    # print(request)
+    # print(request.method)
+    # print(request.path)
+    # print(request.path_info)
+    # print(request.COOKIES)
+    # print(request.META)
+    # print(request.user)
+    # print(request.headers)
+    # print(request.headers['User-Agent'])
+    # return HttpResponse('<h2>Hello World</h2>')
+    return render(request, 'home.html', {})
 
 def patients_view(request, *args, **kwargs):
     print(request)
