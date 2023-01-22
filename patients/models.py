93edca02fbc3184cfce5d0118000efdd90a0616b
sopykt <sopykt09@gmail.com>
2023-01-22 22:14:29 +0630

    add get_absolute_url to patient model and use in templates

--- patients/models.py
+++ patients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Patient(models.Model):
@@ -11,3 +12,7 @@ class Patient(models.Model):
 
     def __str__(self):
         return self.patient_name
+    
+    def get_absolute_url(self): 
+        return reverse("patient_detail_view", kwargs={"id": self.id})
+    
