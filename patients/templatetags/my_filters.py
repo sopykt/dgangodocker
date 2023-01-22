b2df241a3e63eb4b7e03c7c037db770a68b0f458
sopykt <sopykt09@gmail.com>
2023-01-22 20:32:22 +0630

    add custom template filter age and add ipykernel to requirements.txt to work with ipynb in vscode

---
+++ patients/templatetags/my_filters.py
@@ -0,0 +1,18 @@
+from django import template
+from datetime import date
+
+register = template.Library()
+
+@register.filter
+def age(birthdate):
+    today = date.today()
+    age_in_days = (today - birthdate).days
+    age_in_months = int(age_in_days / 30.436875)
+    years, months = divmod(age_in_months, 12)
+    days = today.day - birthdate.day
+    if years == 0 and months != 0:
+        return f"{months} m"
+    elif years == 0 and months == 0:
+        return f"{days} days"
+    else:
+        return f"{years} yrs, {months} m"
