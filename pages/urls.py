f2f348c99ad334b36ea8b156896ad0ee1eb9de88
sopykt <sopykt09@gmail.com>
2023-01-23 18:46:06 +0630

    Home_view inheritance from Templateview

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import home_view, result_view
+from .views import Home_view
 from .extracontext import bg_img, about_context, contact_context
 
 app_name = 'pages'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html', extra_context=bg_img()), name='home'),
+    path('', Home_view.as_view(), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html', extra_context=about_context()), name='about'),
     path('contact/', TemplateView.as_view(template_name='contact.html', extra_context=contact_context()), name='contact')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,25 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.views.generic import TemplateView
+
+# Class based view
+class Home_view(TemplateView):
+    template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        day = datetime.now().day
+        bg_img = 'img/bg/'+ str(day) +'.jpg'
+        context['bg_img'] = bg_img
+        return context
 
 # Create your views here.
-def home_view(request, *args, **kwargs):
-    # print(request)
-    # print(request.method)
-    # print(request.path)
-    # print(request.path_info)
-    # print(request.COOKIES)
-    # print(request.META)
-    # print(request.user)
-    # print(request.headers)
-    # print(request.headers['User-Agent'])
-    # return HttpResponse('<h2>Hello World</h2>')
-    day = datetime.now().day
-    bg_img = 'img/bg/'+ str(day) +'.jpg'
-    context = {
-        'bg_img': bg_img
-    }
-    return render(request, 'home.html', context)
+# The Old home_view
+# def home_view(request, *args, **kwargs):
+#     # print(request)
+#     # print(request.method)
+#     # print(request.path)
+#     # print(request.path_info)
+#     # print(request.COOKIES)
+#     # print(request.META)
+#     # print(request.user)
+#     # print(request.headers)
+#     # print(request.headers['User-Agent'])
+#     # return HttpResponse('<h2>Hello World</h2>')
+#     day = datetime.now().day
+#     bg_img = 'img/bg/'+ str(day) +'.jpg'
+#     context = {
+#         'bg_img': bg_img
+#     }
+#     return render(request, 'home.html', context)
 
 # def patients_view(request, *args, **kwargs):
 #     print(request)
