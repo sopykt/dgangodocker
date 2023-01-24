8e7ae3ad5a805660e7a43987d8002c0e93ed95d6
Soe Paing <sopykt09@gmail.com>
2023-01-24 13:34:39 +0630

    add redirect preloads before destination soepaing.com

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView, RedirectView
-from .views import Home_view
+from .views import Home_view, Redirect_soe
 from .extracontext import bg_img, about_context, contact_context
 
 app_name = 'pages'
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', Home_view.as_view(), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html', extra_context=about_context()), name='about'),
     path('contact/', TemplateView.as_view(template_name='contact.html', extra_context=contact_context()), name='contact'),
-    path('soepaing/', RedirectView.as_view(url='https://soepaing.com'), name='redirect')
+    # path('soepaing/', RedirectView.as_view(url='https://soepaing.com'), name='redirect')
+    path('soepaing/', Redirect_soe.as_view(), name='redirect'),
+    path('soepaings/', RedirectView.as_view(url='https://soepaing.com'), name='destination')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 # Class based view
 class Home_view(TemplateView):
@@ -13,6 +13,22 @@ class Home_view(TemplateView):
         bg_img = 'img/bg/'+ str(day) +'.jpg'
         context['bg_img'] = bg_img
         return context
+
+
+# Redirect View
+class Redirect_soe(RedirectView):
+    permanent = False
+    query_string = True
+    pattern_name = 'pages:destination'
+    def get_redirect_url(self, *args, **kwargs):
+        request_instance = {
+            'user': str(self.request.user),
+            'agent': self.request.headers['User-Agent'],
+            'clicked': 'redirect'
+        }
+        print(request_instance)
+        return super().get_redirect_url(*args, **kwargs)
+
 
 # Create your views here.
 # The Old home_view
@@ -42,4 +58,3 @@ class Home_view(TemplateView):
 def result_view(request, *args, **kwargs):
     ss = request.GET.get('ss', None)
     return render(request, 'home.html', {'ss': ss})
-
