f2742cb3b40aa48a586e4f922f1e026e14c07d2b
Soe Paing <sopykt09@gmail.com>
2023-01-24 11:50:03 +0630

    add simple redirect to soepaing.com

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from .views import Home_view
 from .extracontext import bg_img, about_context, contact_context
 
@@ -7,5 +7,6 @@ app_name = 'pages'
 urlpatterns = [
     path('', Home_view.as_view(), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html', extra_context=about_context()), name='about'),
-    path('contact/', TemplateView.as_view(template_name='contact.html', extra_context=contact_context()), name='contact')
+    path('contact/', TemplateView.as_view(template_name='contact.html', extra_context=contact_context()), name='contact'),
+    path('soepaing/', RedirectView.as_view(url='https://soepaing.com'), name='redirect')
 ]
