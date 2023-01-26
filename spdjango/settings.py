77aec6103f945d3de8ec3da94b3fbd26a9625be2
Soe Paing <sopykt09@gmail.com>
2023-01-26 17:30:51 +0630

    add os.environ to settings to get secret key

--- spdjango/settings.py
+++ spdjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5r8iu3o&dc3-=5)&x$f=n35-)*c$08v$=m)7p@zcp2()g05sir"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-5r8iu3o&dc3-=5)&x$f=n35-)*c$08v$=m)7p@zcp2()g05sir")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
