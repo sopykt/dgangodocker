ca9eb89e0512ee928d1c08a4dd141ed2d517c20d
sopykt <sopykt09@gmail.com>
2023-02-05 13:16:42 +0630

    add ec2localip to allowed hosts

--- spdjango/settings_prod.py
+++ spdjango/settings_prod.py
@@ -35,7 +35,15 @@ if os.environ.get("ALLOWED_HOSTS") is not None:
     except Exception as e:
         print("Cant set ALLOWED_HOSTS, using default instead")
 
+import requests
+EC2_PRIVATE_IP  =   None
+try:
+    EC2_PRIVATE_IP  =   requests.get('http://169.254.169.254/latest/meta-data/local-ipv4', timeout = 0.01).text
+except requests.exceptions.RequestException:
+    pass
 
+if EC2_PRIVATE_IP:
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
 
 # Application definition
 
