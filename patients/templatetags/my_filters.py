7c84d403d4e293a57ff0823dfdd00725cc5461c3
Soe Paing <sopykt09@gmail.com>
2023-03-04 23:08:17 +0630

    fix days error in age filter

--- patients/templatetags/my_filters.py
+++ patients/templatetags/my_filters.py
@@ -9,10 +9,10 @@ def age(birthdate):
     age_in_days = (today - birthdate).days
     age_in_months = int(age_in_days / 30.436875)
     years, months = divmod(age_in_months, 12)
-    days = today.day - birthdate.day
+    # days = today.day - birthdate.day
     if years == 0 and months != 0:
         return f"{months} m"
     elif years == 0 and months == 0:
-        return f"{days} days"
+        return f"{age_in_days} days"
     else:
         return f"{years} yrs, {months} m"
